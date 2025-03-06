5d9015ad4cc3fff43219b9553aa7739ad7ab0753
bishal9990 <bishalkharel9990@gmail.com>
2025-03-06 14:19:39 +0545

    condition.py

--- 15condition.py
+++ 15condition.py
@@ -1,26 +1,26 @@
-# # age = int(input("Enter your age: "))
-# # print(f"You have entered {age}")
+age = int(input("Enter your age: "))
+ print(f"You have entered {age}")
 
-# # if age>= 18:
-# #     print("You are voter: ")
-# #     print("You can vote: ")
+ if age>= 18:
+     print("You are voter: ")
+    print("You can vote: ")
 
-# # else:
-# #     print("You are not voter: ")
-# #     print("You cant vote: ")
+ else:
+    print("You are not voter: ")
+   print("You cant vote: ")
 
 
 # #temperature_example
 
-# temp = 15
+temp = 15
 
-# if temp >=25:
-#     print("It is so hot")
+ if temp >=25:
+   print("It is so hot")
 
-# elif temp>= 15 and temp<25:
-#     print("Temperature is okay")
-# else:
-#     print("Its cold")
+elif temp>= 15 and temp<25:
+    print("Temperature is okay")
+ else:
+   print("Its cold")
 
 
 
