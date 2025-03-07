e98351392c2502d3fcb21eddc0ad8341e4f7e3f0
bishal9990 <bishalkharel9990@gmail.com>
2025-03-07 13:07:02 +0545

    Add files via upload

---
+++ 21tuple.py
@@ -0,0 +1,21 @@
+colors = ("red", "green", "black")
+print(colors)
+
+
+#combining tuples
+#create two tuples
+odd_numbers = (1,3,5)
+even_numbers = (2,4,6)
+
+#combine the tuples
+all_numbers = odd_numbers + even_numbers
+print(f"Combined Tuple: {all_numbers}")
+
+#tuple length              len-> length
+#create a  tuple
+fruit_tuple = ("apple", "banana", "cherry")
+#get the length of the tuple
+num_fruits = len(fruit_tuple)
+print(f"Number of fruits:  {num_fruits}")
+
+
