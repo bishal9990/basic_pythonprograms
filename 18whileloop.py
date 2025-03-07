b520b34af1478376089c234bdad2bf67a62a3a5d
bishal9990 <bishalkharel9990@gmail.com>
2025-03-07 11:37:03 +0545

    Add files via upload

---
+++ 18whileloop.py
@@ -0,0 +1,22 @@
+num = 1
+
+
+while num<=100:
+    print(num)
+    num+=1
+
+
+#whileloop countdown timer
+
+countdown = 10
+while countdown > 0:
+     print(countdown)
+     countdown -= 1
+print("Countdown complete!")
+
+#validating user password
+
+password = ""
+while password != "python123":
+     password = input("Enter the password ")
+print("Access granted ")
