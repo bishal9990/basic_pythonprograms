f25fc245b668a10084cc00e4341d6b3258a8edb5
bishal9990 <bishalkharel9990@gmail.com>
2025-03-09 06:51:51 +0545

    Add files via upload

---
+++ 22set.py
@@ -0,0 +1,11 @@
+fruits = {"apple", "banana", "mango"}
+
+
+fruits.add("orange")  #add the items
+fruits.remove("banana")  #remove the items
+#print(fruits)
+
+for fruit in fruits:
+    print(fruit)
+
+    
